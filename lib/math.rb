322f0c57c047b59c2a9c39b3783497bc79bec0ff
Paula Tulis <tulis.paula@gmail.com>
2019-02-27 02:16:12 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -2,19 +2,24 @@ def addition(num1=5, num2=4)
   return num1 + num2
 end
 
-def subtraction(num1, num2)
+def subtraction(num1=10, num2=5)
+  return num1 - num2
 end
 
-def division(num1, num2)
+def division(num1=50, num2=2)
+  return num1/num2
 end
 
-def multiplication(num1, num2)
+def multiplication(num1=4, num2=30)
+  return num1 * num2
 end
 
-def modulo(num1, num2)
+def modulo(num1=34, num2=5)
+  return num1 % num2
 end
 
-def square_root(num)
+def square_root(num=81)
+  return math.sqrt(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
